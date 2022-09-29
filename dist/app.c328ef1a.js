a2cb4155d6538b9874950af45cc93b481029fa11
bbAn <damaskrose83@gmail.com>
2022-09-29 12:59:23 +0900

    CH03_02. 12줄의 코드로 시작하기

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,7 +118,19 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+var ajax = new XMLHttpRequest();
+ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false);
+ajax.send();
+var newsFeed = JSON.parse(ajax.response);
+var ul = document.createElement('ul');
 
+for (var i = 0; i < 10; i++) {
+  var li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
 },{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
